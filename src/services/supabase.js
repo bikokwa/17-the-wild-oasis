b5c053fd2055ebfca65891567abea4b85d1ad284
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-06-11 20:11:17 +0300

    upload images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wxgxgtkdcnzlnwbvizhd.supabase.co";
+export const supabaseUrl = "https://wxgxgtkdcnzlnwbvizhd.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4Z3hndGtkY256bG53YnZpemhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MTcwODUsImV4cCI6MjAzMjQ5MzA4NX0.z14FJh_k2BYq7FNcbiyPV72ZerR88-RQwzm-NKVhEM4";
 const supabase = createClient(supabaseUrl, supabaseKey);
